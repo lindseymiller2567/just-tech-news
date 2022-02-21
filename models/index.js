afaf13fbed8f1231fdbe72b856b37eac6b0dd67e
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-02-20 20:16:37 -0600

    created post model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User'); // import the User model
+const Post = require('./Post')
 
-module.exports = { User }; // and export an object with User as a property
+module.exports = { User, Post }; // and export an object with User as a property
